b1003d3cf6ea29272fa99258799a3440db94e853
trentofflarrabee <58488328+trentofflarrabee@users.noreply.github.com>
2026-08-05 12:11:27 -0400

    Update slim-volume.php

--- slim-volume.php
+++ slim-volume.php
@@ -7,6 +7,8 @@
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * Author: Slim Volume
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI: https://github.com/trentofflarrabee/slim-volume
  * Text Domain: slim-volume
  */
